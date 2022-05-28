55ad496848862c1c308bcaabb117836b9157e9b3
ChuWenFeng <1721306020@qq.com>
2022-05-28 23:40:20 +0800

    m

--- leetcode_rs/src/solved/P330.rs
+++ leetcode_rs/src/solved/P330.rs
@@ -9,11 +9,12 @@ use super::Solution;
 impl Solution {
     pub fn min_patches(nums: Vec<i32>, n: i32) -> i32 {
         let mut i = 0;
-        let mut x = 1;
+        let mut x:i64 = 1;
         let mut ans = 0;
+        let mut n = n as i64;
         while x<=n{
-            if i < nums.len() && nums[i] <= x{
-                x+=nums[i];
+            if i < nums.len() && nums[i] as i64 <= x{
+                x+=nums[i] as i64;
                 i+=1;
             }else{
                 x*=2;
@@ -25,3 +26,11 @@ impl Solution {
 }
 // @lc code=end
 
+#[test]
+fn test(){
+    let nums = vec![1,2,31,33];
+    let n = 2147483647;
+    let mut ans = 28;
+    let mut res = Solution::min_patches(nums, n);
+    assert_eq!(res,ans);
+}
